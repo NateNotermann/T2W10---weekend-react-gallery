debda4c0418889afa9be1faf1b86f2c921b685d5
Nate Notermann <nate.notermann@gmail.com>
2022-07-10 20:35:45 -0500

    like button works. working on gallery list

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -3,30 +3,39 @@ import { useState } from "react";
 
 
 
-let likes = 0;
-let items = ['test thing 1','test thing 1' ];
+// let likes = 0;
+// let items = ['test thing 1','test thing 1' ];
 
-
-function LikeFunctction() {
-    // setLike = likes +=1;
-    likes += 1;
-    console.log('liked', likes, 'times');
-}
-
-
-
+// function LikeFunctction() {
+//     // setLike = likes +=1;
+//     setLikes( likes +1 );
+//     console.log('liked', likes, 'times');
+// }
 
 function GalleryItem() {
+    const [likes, setLikes] = useState(0); // MUST be in FUNCTION
 
-    
+    const LikeButton = () => {
+        setLikes(likes +1);
+        console.log(likes)
+    }
+    const UnLikeButton = () => {
+        setLikes(likes -1);
+        console.log(likes)
+    }
+    const ResetLikeButton = () => {
+        setLikes(0);
+        console.log(likes)
+    }    
     return (
         <>
 
             <div id="item">
                 <img src="images/biking.jpg" />
                 <br></br>
-                <button onClick={(event) => LikeFunctction()
-                }>LIKE</button>
+                <button onClick={LikeButton}>LIKE</button>
+                <button onClick={UnLikeButton}>UNLIKE</button>
+                <button onClick={ResetLikeButton}>RESET</button>
                 <h3># of Likes:{likes}</h3>
 
             </div>
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,39 @@
+// import {useState } from 'react';
+
+function GalleryListFunction (){
+
+    const Gallery = [
+        {thing: 'biking',
+        link: 'images/biking.jpg'},
+        {thing: 'car',
+        link: 'images/car.jpg'},
+        {thing: 'coffee',
+        link: 'images/coffee.jpg'},
+        {thing: 'friends',
+        link: 'images/friends.jpg'},
+        {thing: 'internet',
+        link: 'images/internet.jpg'},
+        {thing: 'movies',
+        link: 'images/movies.jpg'},
+        {thing: 'music',
+        link: 'images/music.jpg'},
+        {thing: 'food',
+        link: 'images/food.jpg'},
+        {thing: 'running',
+        link: 'images/running.jpg'},
+        {thing: 'streaming',
+        link: 'images/streaming.jpg'},
+        {thing: 'travel',
+        link: 'images/travel.jpg'},
+    ];
+
+    return (
+        <>
+        <h4> GalleryList TEST</h4>
+        </>
+    )
+
+}
+
+
+export default GalleryListFunction;
